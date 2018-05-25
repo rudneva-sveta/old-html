d1884da4e07a2920e01f83d339a6944d79b64688
rudneva-sveta <32402310+rudneva-sveta@users.noreply.github.com>
2018-05-25 19:01:44 +0300

    САЙТ КАВЗ
    для портфолио

---
+++ kavz/popup-calculation/mailer/smart.php
@@ -0,0 +1,43 @@
+ <?php 
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'svetlana_107@inbox.ru';                 // Наш логин
+$mail->Password = 'hf,jnfdrjvgfybb1';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('svetlana_107@inbox.ru', 'КАВЗ');   // От кого письмо 
+$mail->addAddress('rudneva.s@bk.ru');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с сайта';
+$mail->Body    = '
+	Пользователь оставил свои данные <br> 
+	Имя: ' . $name . ' <br>
+	Телефон: ' . $phone . '';
+$mail->AltBody = 'перезвонить в течении 30 минут';
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
